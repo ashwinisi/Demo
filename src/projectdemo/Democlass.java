099b523f3523e14bd4a63f95e103fddb173d2b4e
ashwini singh <ashwinisi@cybage.com>
2020-01-24 13:58:02 +0530

    Files commit

---
+++ src/projectdemo/Democlass.java
@@ -0,0 +1,10 @@
+package projectdemo;
+
+public class Democlass {
+
+	public static void main(String[] args) {
+		System.out.println("hello demo class");
+        System.out.println("hello Java");
+	}
+
+}
